ff8c26b8e1589c91db47594ee629f21239e8e7a3
Jason Frame <jwf@jasonframe.co.uk>
2021-06-15 20:38:02 +0100

    tabs -> spaces

--- demo/main.js
+++ demo/main.js
@@ -2,42 +2,42 @@ const SerialPort = require('@serialport/stream');
 SerialPort.Binding = require('..');
 
 document.addEventListener('DOMContentLoaded', () => {
-	const decoder = new TextDecoder();
+    const decoder = new TextDecoder();
 
-	let port;
+    let port;
 
-	document.querySelector('button[name="open"]').onclick = async () => {
-		if (port) {
-			return;
-		}
-		
-		const nativePort = await navigator.serial.requestPort();
+    document.querySelector('button[name="open"]').onclick = async () => {
+        if (port) {
+            return;
+        }
+        
+        const nativePort = await navigator.serial.requestPort();
 
-		port = new SerialPort(nativePort, {baudRate: 115200});
+        port = new SerialPort(nativePort, {baudRate: 115200});
 
-		port.on('open', () => {
-			console.log("port open!");
-		});
+        port.on('open', () => {
+            console.log("port open!");
+        });
 
-		port.on('data', (data) => {
-			console.log("recv", JSON.stringify(decoder.decode(data)));
-		});
+        port.on('data', (data) => {
+            console.log("recv", JSON.stringify(decoder.decode(data)));
+        });
 
-		port.on('end', () => {
-			console.log("stream ended");
-		});
+        port.on('end', () => {
+            console.log("stream ended");
+        });
 
-		setInterval(() => {
-			port.write("HELLO\r\n");
-		}, 100);
-	};
+        setInterval(() => {
+            port.write("HELLO\r\n");
+        }, 100);
+    };
 
-	document.querySelector('button[name="close"]').onclick = async () => {
-		if (!port) {
-			return;
-		}
-		port.close();
-		port = null;
-		return;
-	};
+    document.querySelector('button[name="close"]').onclick = async () => {
+        if (!port) {
+            return;
+        }
+        port.close();
+        port = null;
+        return;
+    };
 });
